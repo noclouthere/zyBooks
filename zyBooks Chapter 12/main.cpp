e084a4337c7fd10bbe02aafc4f3430276af588e6
noclouthere <40571849+noclouthere@users.noreply.github.com>
2019-02-28 09:51:52 -0700

    Update main.cpp

--- zyBooks Chapter 12/main.cpp
+++ zyBooks Chapter 12/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-#include "Employee.h"
+#include <vector>
+#include "HourlyEmployee.h"
+#include "SalariedEmployee.h"
+
 using namespace std;
 
 int main() {
-
-
-    Employee emp1(37, "Joe Brown", "123 Main St.", "123-6788");
-    Employee emp2(37, "Sam Jones", "45 East State", "661-9000");
-    Employee emp3(15, "Mary Smith", "12 High Street", "401-8900");
+    //Create a vector of Employee pointers names employees
+    vector<Employee*> employees;
+    //Add the four employees from the project description to the employees
+    //Vector
+    employees.push_back(new HourlyEmployee(1, "H. Potter", "Privet Drive", "201-9090",40,12));
+    employees.push_back(new SalariedEmployee(2, "A. Dumbledore", "Hogwarts", "803-1230",1200));
+    employees.push_back(new HourlyEmployee(3,"Ron Weasley", "The Burrow", "892-20000",40,10));
+    employees.push_back(new SalariedEmployee(4, "R. Hagrid", "Hogwarts", "910-8765", 1000));
 
 
     char response = 'z';
@@ -16,7 +22,7 @@ int main() {
         cout << "This program has two options:" << endl;
         cout << "1 - Create a data file, or" << endl;
         cout <<"2 - Read data from a file and print paychecks." << endl;
-        cout << "Please enter (1) to create a file or (2) to print checks:" << endl;
+        cout << "Please enter <1> to create a file or (2) to print checks:" << endl;
         cin >> response;
 
 
@@ -27,11 +33,10 @@ int main() {
             string filename;
             cin >> filename;
             theFile.open(filename);
-            emp1.write(theFile);
-            emp2.write(theFile);
+            (employees[0])->write(theFile);
+            (employees[1])->write(theFile);
+            (employees[3])->write(theFile);
 
-            cout << "Data file created ... you can now run option 2." << endl;
-            response = 'q';
 
 
 
@@ -39,19 +44,19 @@ int main() {
             theFile.close();
         }
         else if(response == '2') {
-            Employee emp4, emp5, emp6;
-            cin >> response;
+            HourlyEmployee emp4, emp5, emp6;
             ifstream theFile;
+            theFile.open("Bob.mre");
 
-            emp4 = emp4.read(theFile);
-            emp5 = emp5.read(theFile);
-            cout << "Please enter a file name:" << endl;
-            printCheck(emp4);
-            printCheck(emp5);
+            emp4.read(theFile);
+            emp4.printCheck();
 
-            response = 'q';
+            theFile.close();
+
 
         }
 
-    }
+    }while(response!= 'q');
+
+    return 0;
 }
